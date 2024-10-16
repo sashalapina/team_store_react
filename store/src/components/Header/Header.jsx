fad388d0246cbd124215a3634ad63581910176a3
Sasha Lapina <lapinasasha@gmail.com>
2024-10-17 02:02:16 +0300

    add login

--- store/src/components/Header/Header.jsx
+++ store/src/components/Header/Header.jsx
@@ -1,12 +1,14 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { fetchCategories } from '../../api/fakeStoreApi';
+import Login from "../SignIn/Login";
 import './Header.css'
 
 function Header() {
     const navigate = useNavigate();
     const [categories, setCategories] = useState([]);
     const [error, setError] = useState(null);
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
   const goToCartButton = () => {
     navigate('/cart');
@@ -16,6 +18,9 @@ function Header() {
     const link_category = category.replace(/ /g, '_');
     navigate(`/products/${link_category}`);
   }
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
 
     useEffect(() => {
         const getCategories = async () => {
@@ -30,10 +35,11 @@ function Header() {
     },[])
 
     return (
+        <>
         <header className="header">
-            <div className="header__contsiner">
+            <div className="header__container">
                 <a className="header__link" href="/">
-                    <img className="header__link-img" src="/logo.png" alt="" />
+                    <img className="header__link-img" src="/logo.png" alt="Fake Store Logo" />
                     Fake Store
                 </a>
                 {categories.map((category) => (
@@ -44,17 +50,26 @@ function Header() {
                         </ul>
                     </>
                 ))}
-                <button className="header__button" onClick={goToCartButton}>
-                    <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M3.86376 16.4552C3.00581 13.0234 2.57684 11.3075 3.47767 10.1538C4.3785 9 6.14721 9 9.68462 9H14.3153C17.8527 9 19.6214 9 20.5222 10.1538C21.4231 11.3075 20.9941 13.0234 20.1362 16.4552C19.5905 18.6379 19.3176 19.7292 18.5039 20.3646C17.6901 21 16.5652 21 14.3153 21H9.68462C7.43476 21 6.30983 21 5.49605 20.3646C4.68227 19.7292 4.40943 18.6379 3.86376 16.4552Z" stroke-width="1.5"/>
-                        <path d="M19.5 9.5L18.7896 6.89465C18.5157 5.89005 18.3787 5.38775 18.0978 5.00946C17.818 4.63273 17.4378 4.34234 17.0008 4.17152C16.5619 4 16.0413 4 15 4M4.5 9.5L5.2104 6.89465C5.48432 5.89005 5.62128 5.38775 5.90221 5.00946C6.18199 4.63273 6.56216 4.34234 6.99922 4.17152C7.43808 4 7.95872 4 9 4" stroke-width="1.5"/>
-                        <path d="M9 4C9 3.44772 9.44772 3 10 3H14C14.5523 3 15 3.44772 15 4C15 4.55228 14.5523 5 14 5H10C9.44772 5 9 4.55228 9 4Z" stroke-width="1.5"/>
-                        <path d="M4.5 18L12 9M19.5 18L12.5 9.5M4.5 10L12 21L19.5 10" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
-                </button>
+                <div className="header__buttons_group">
+                    <button className="header__button-user-profile" onClick={openModal}>
+                        <svg width="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <circle cx="12" cy="6" r="4" stroke-width="1.5"/>
+                            <path d="M19.9975 18C20 17.8358 20 17.669 20 17.5C20 15.0147 16.4183 13 12 13C7.58172 13 4 15.0147 4 17.5C4 19.9853 4 22 12 22C14.231 22 15.8398 21.8433 17 21.5634" stroke-width="1.5" stroke-linecap="round"/>
+                        </svg>
+                    </button>
+                    <button className="header__button" onClick={goToCartButton}>
+                        <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M3.86376 16.4552C3.00581 13.0234 2.57684 11.3075 3.47767 10.1538C4.3785 9 6.14721 9 9.68462 9H14.3153C17.8527 9 19.6214 9 20.5222 10.1538C21.4231 11.3075 20.9941 13.0234 20.1362 16.4552C19.5905 18.6379 19.3176 19.7292 18.5039 20.3646C17.6901 21 16.5652 21 14.3153 21H9.68462C7.43476 21 6.30983 21 5.49605 20.3646C4.68227 19.7292 4.40943 18.6379 3.86376 16.4552Z" stroke-width="1.5"/>
+                            <path d="M19.5 9.5L18.7896 6.89465C18.5157 5.89005 18.3787 5.38775 18.0978 5.00946C17.818 4.63273 17.4378 4.34234 17.0008 4.17152C16.5619 4 16.0413 4 15 4M4.5 9.5L5.2104 6.89465C5.48432 5.89005 5.62128 5.38775 5.90221 5.00946C6.18199 4.63273 6.56216 4.34234 6.99922 4.17152C7.43808 4 7.95872 4 9 4" stroke-width="1.5"/>
+                            <path d="M9 4C9 3.44772 9.44772 3 10 3H14C14.5523 3 15 3.44772 15 4C15 4.55228 14.5523 5 14 5H10C9.44772 5 9 4.55228 9 4Z" stroke-width="1.5"/>
+                            <path d="M4.5 18L12 9M19.5 18L12.5 9.5M4.5 10L12 21L19.5 10" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </button>
+                </div>
             </div>
-            
         </header>
+        <Login isOpen={isModalOpen} closeModal={closeModal} />
+        </>
     )
 }
 
---
+++ store/src/components/SignIn/Login.jsx
@@ -0,0 +1,108 @@
+import { useEffect, useState } from "react";
+import './Login.css'
+
+function Login({ isOpen, closeModal }) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const sessionId = localStorage.getItem('session_id');
+        if (sessionId) {
+            setIsLoggedIn(true);
+        }
+    },[]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();  // Останавливаем стандартную отправку формы
+
+        const loginData = {
+            username: email, // В этом API используется поле "username"
+            password: password
+        };
+
+        try {
+            const response = await fetch('https://fakestoreapi.com/auth/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(loginData),
+            });
+
+            const data = await response.json();
+
+            if (response.ok) {
+                const token = data.token;
+                localStorage.setItem('session_id', token);
+                setIsLoggedIn(true);
+            } else {
+                setError('Login failed: ' + data.message);
+                console.error('Login failed', data);
+            }
+        } catch (error) {
+            console.error('An error occurred', error);
+            setError('An error occurred. Please try again');
+        }
+    };
+
+    const handleLogOut = () => {
+        localStorage.removeItem('session_id');
+        closeModal();
+        window.location.reload();
+    }
+
+    if (!isOpen) return null;
+
+    return (
+        <>
+        <div className="modal-overlay" onClick={closeModal}></div> {/* Overlay для затемнения */}
+        <div className="modal">
+            <div className="modal-content">
+                {isLoggedIn ? (
+                    <div className="user-profile">
+                        <h1 className="user-profile-title">User Profile</h1>
+                        <p className="user-profile-text">Your sign in was successful</p>
+                        <div className="user-profile-group-btn">
+                            <button className="logout-btn" onClick={handleLogOut}>Log out</button>
+                        </div>
+                    </div>
+                ) : (
+                    <>
+                    <h1 className="login-title">Sign In</h1>
+                    {error && <p className="error-message">{error}</p>}
+                    <form onSubmit={handleSubmit}>
+                        <label htmlFor="email">
+                            Username:
+                            <input
+                                type="text"
+                                id="email"
+                                className="input"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                required
+                            />
+                        </label>
+                        <label htmlFor="password">
+                            Password:
+                            <input
+                                type="password"
+                                id="password"
+                                className="input"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                required
+                            />
+                        </label>
+                        <button type="submit" className="login-submit-btn">Sign In</button>
+                    </form>
+                    </>
+                )}
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Login;
