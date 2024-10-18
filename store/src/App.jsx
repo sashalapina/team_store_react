ad51c6e31e4148af8fc731f2a1f761e1a9625ab6
rubis-design <rubis.smm@gmail.com>
2024-10-18 08:07:58 +0300

    fixing bugs

--- store/src/App.jsx
+++ store/src/App.jsx
@@ -3,20 +3,17 @@ import ProductCardPage from './pages/ProductCardPage';
 import ProductsListPage from './pages/ProductsListPage';
 import ProductCategoryPage from './pages/ProductCategoryPage';
 import CartPage from './pages/CartPage';
-import { CartProvider } from './components/CartContext';
 
 const App = () => {
   return (
-    <CartProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<ProductsListPage />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/product/:id" element={<ProductCardPage />} />
-          <Route path="/products/:category" element={<ProductCategoryPage />} />
-        </Routes>
-      </Router>
-    </CartProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<ProductsListPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/product/:id" element={<ProductCardPage />} />
+        <Route path="/products/:category" element={<ProductCategoryPage />} />
+      </Routes>
+    </Router>
   );
 };
 
